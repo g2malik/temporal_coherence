55e2db108b53f0ff38507672a49ea7031a5aa41e
g2malik <gagankumar2k@gmail.com>
2021-07-02 21:52:47 -0400

    Add comments/ find location of max coherence

--- no_UMZ_pdf.py
+++ no_UMZ_pdf.py
@@ -1,11 +1,12 @@
 # --- 
-# aims: reads data on number of UMZs and plots pdf
+# aims: reads data on number of UMZs and gives a list of the number of frames a particular number of UMZs lasts
 # calls: none
 # modefication history: gmalik, July, 2021; 
 
 # --------------------------------
 # import libraries 
 
+from types import ModuleType
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl 
@@ -19,32 +20,35 @@ two = []
 three = []
 four = []
 five = []
-six = []
+six = [] #
 
+most_coherent = []
 UMZ_order = []
+
 for line in f:
-    if (line.startswith("z") or line.startswith("t"))  == True:
+    if (line.startswith("z") or line.startswith("t"))  == True: #If heading analyse the array of # of UMZs
         time = 1
         for i in range(len(UMZ_order)):
             if i!=0 and UMZ_order[i] == UMZ_order[i-1]:
                 time+=1
             if ((UMZ_order[i] != UMZ_order[i-1] or i==(len(UMZ_order)-1)) and time!=1):
-                eval(num2words(UMZ_order[i-1])).append(time)
+                eval(num2words(UMZ_order[i-1])).append(time) #If not the same # anymore or last value, add value to array 
+                if time>=7:
+                    most_coherent.append(old_snap) #Identifies the locations where the number of UMZs stays constant for 7 frames
                 time = 1
-
-        UMZ_order = []
-        pass 
+        old_snap = line
+        UMZ_order = [] 
     else:
         lst = line.split()
         UMZs_str = lst[0]
-        UMZ_order.append(int(UMZs_str))
+        UMZ_order.append(int(UMZs_str)) #Build array of all the # of UMZs in this frame
 
-print(np.mean(one))
-print(np.mean(two))
-print(np.mean(three))
-print(np.mean(four))
-print(np.mean(five))
+#print(np.mean(two))
+#print(np.mean(three))
+#print(np.mean(four))
+#print(np.mean(five))
 
+print(most_coherent)
 
 
 
